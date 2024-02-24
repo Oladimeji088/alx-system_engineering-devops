8c2574e4f628500551336a13630202ac78312768
Rufai Abdulwasii Oladimeji <rufaiwasiu5@gmail.com>
2024-02-24 06:53:39 -0800

    Making Changes

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,13 @@
 # install flask using puppet from pip3
 
-package { 'flask':
-    ensure   => '2.1.0',
-    provider => 'pip3',
+package { 'python3-pip':
+  ensure => installed,
 }
+
+exec { 'install_flask':
+  command     => '/usr/bin/pip3 install Flask',
+  path        => ['/usr/bin'],
+  refreshonly => true,
+}
+
+
